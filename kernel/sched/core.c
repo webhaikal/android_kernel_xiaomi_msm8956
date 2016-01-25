5e183f671c45617b9eab8337581d2bfe45cf2b32
Shoaib0597 <Shoaib0595@gmail.com>
2016-01-25 16:23:49 +0530

    Optimized task_sched_runtime for upto 20% increase in Performance
    Conflicts:
    	kernel/sched/core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5229,6 +5229,7 @@ unsigned long long task_sched_runtime(struct task_struct *p)
  if (!p->on_cpu)
  return p->se.sum_exec_runtime;
 #endif
+
 	rq = task_rq_lock(p, &flags);
 	ns = p->se.sum_exec_runtime + do_task_delta_exec(p, rq);
 	task_rq_unlock(rq, p, &flags);
