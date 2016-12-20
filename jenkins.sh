4a05b2867330b354ab36fc7213d0cccbd9b27715
Haikal Izzuddin <heywhite69@gmail.com>
2016-12-20 03:40:24 +0000

    Update jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -2,8 +2,8 @@
 KERNEL_DIR=$PWD
 KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
 DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
-version = "1.0"
-TC = "uber"
+VERSION="1.0"
+TC="uber"
 
 export USE_CCACHE=1
 export ARCH=arm64
@@ -11,8 +11,8 @@ export SUBARCH=arm64
 export KBUILD_BUILD_USER="Haikal Izzuddin"
 export KBUILD_BUILD_HOST="haikalizz"
 STRIP="/home/heywhite69/sensei/uber4.9/bin/aarch64-linux-android-strip"
-BUILD_DIR=$KERNEL_DIR/../output
-MODULES_DIR="${KERNEL_DIR}/../output/modules"
+BUILD_DIR=$KERNEL_DIR/../output2
+MODULES_DIR="${KERNEL_DIR}/../output2/modules"
 
 export CROSS_COMPILE="/home/heywhite69/sensei/uber4.9/bin/aarch64-linux-android-"
 make cyanogenmod_kenzo_defconfig
@@ -30,8 +30,14 @@ rm $BUILD_DIR/dt.img
 cp -vr $KERNEL_DIR/arch/arm64/boot/Image $BUILD_DIR/tools/Image
 cp $KERNEL_DIR/arch/arm64/boot/dt.img $BUILD_DIR/tools/dt.img
 cd $BUILD_DIR
-zipfile="SenseiKenzo-$version+$TC-$(date +"%Y-%m-%d(%I.%M%p)").zip"
+zipfile="SenseiKenzo-$VERSION+$TC-$(date +"%Y-%m-%d(%I.%M%p)").zip"
 echo $zipfile
-zip -r9 $zipfile * -x README
+zip -r9 $zipfile * -x README *.zip
+
+outdir=(/usr/share/nginx/html/SenseiKernel/SenseiKenzo*)
+if [ ${outdir[@]} -gt 2]; then
+sudo ls -t | sed -e '1,3d' | xargs -d '\n' sudo rm
+fi
+
 sudo mv $BUILD_DIR/$zipfile /usr/share/nginx/html/SenseiKernel
 
