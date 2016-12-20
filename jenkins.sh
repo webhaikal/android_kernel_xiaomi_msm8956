9df6ea061bad1eba76e8306ed2f86d9eccdff797
Haikal Izzuddin <heywhite69@gmail.com>
2016-12-20 03:05:40 +0000

    SenseiKenzo: Bring up support

---
+++ jenkins.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/arch/arm64/boot/Image
+DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
+version = "1.0"
+TC = "uber"
+
+export USE_CCACHE=1
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="Haikal Izzuddin"
+export KBUILD_BUILD_HOST="haikalizz"
+STRIP="/home/heywhite69/sensei/uber4.9/bin/aarch64-linux-android-strip"
+BUILD_DIR=$KERNEL_DIR/../output
+MODULES_DIR="${KERNEL_DIR}/../output/modules"
+
+export CROSS_COMPILE="/home/heywhite69/sensei/uber4.9/bin/aarch64-linux-android-"
+make cyanogenmod_kenzo_defconfig
+make -j5 CONFIG_NO_ERROR_ON_MISMATCH=y
+
+if ! [ -s $KERN_IMG ];
+	then
+		echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+		exit 1
+fi
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+
+rm $BUILD_DIR/Image
+rm $BUILD_DIR/dt.img
+cp -vr $KERNEL_DIR/arch/arm64/boot/Image $BUILD_DIR/tools/Image
+cp $KERNEL_DIR/arch/arm64/boot/dt.img $BUILD_DIR/tools/dt.img
+cd $BUILD_DIR
+zipfile="SenseiKenzo-$version+$TC-$(date +"%Y-%m-%d(%I.%M%p)").zip"
+echo $zipfile
+zip -r9 $zipfile * -x README
+sudo mv $BUILD_DIR/$zipfile /usr/share/nginx/html/SenseiKernel
+
